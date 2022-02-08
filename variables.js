4d6abdcde236b97087e89d9d847815177bf0ebe9
Ana Bennett <anabennett1111@gmail.com>
2022-02-08 17:58:46 -0500

    annotate one of the files

--- variables.js
+++ variables.js
@@ -1,5 +1,5 @@
 var favoritePet = "dog"
-var favoritePlant = "cactus"
+var favoritePlant = "cactus"  // I couldn't possibly pick one favorite plant.
 var age = 32
 var luckyNumber = 11
 var stillWatching = true
